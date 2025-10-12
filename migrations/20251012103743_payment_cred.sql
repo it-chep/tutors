6f2129316ee80c99d1ffe254526ee3e44ba44787
duzyanov <duzyanov@ozon.ru>
2025-10-12 14:53:14 +0300

    support multiple payment users

--- migrations/20251012103743_payment_cred.sql
+++ migrations/20251012103743_payment_cred.sql
@@ -2,9 +2,9 @@
 -- +goose StatementBegin
 create table payment_cred
 (
-    admin_id     bigint not null,
-    user_pay     text   not null,
-    password_pay text   not null
+    admin_id     bigint primary key not null,
+    user_pay     text               not null,
+    password_pay text               not null
 );
 -- +goose StatementEnd
 
