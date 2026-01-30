7d22052acf204e02f158e6900a43103836827f86
Maksim Necheporuk <mnecheporuka@ozon.ru>
2026-01-30 19:20:52 +0300

    N платежек

--- migrations/20260130144040_add_n_payments.sql
+++ migrations/20260130144040_add_n_payments.sql
@@ -14,6 +14,16 @@ alter table payment_cred
 
 alter table students
     add column payment_id bigint;
+
+insert into permissions(id, name, url, description)
+values (44, 'Получение всех платежек админка', '/admin/payments', 'Получение всех платежек админка'),
+       (45, 'Смена платежки у пользователя', '/admin/students/{id}/change_payment', 'Смена платежки у пользователя');
+
+insert into roles_permissions(id, role_id, permission_id)
+values (122, 2, 44),
+       (123, 2, 45),
+       (124, 4, 44),
+       (125, 4, 45);
 -- +goose StatementEnd
 
 -- +goose Down
