ba413299cad576276e2de7771d2e9c5b9e7d832d
Maksim Necheporuk <mnecheporuka@ozon.ru>
2026-01-30 17:57:21 +0300

    N платежек

---
+++ migrations/20260130144040_add_n_payments.sql
@@ -0,0 +1,22 @@
+-- +goose Up
+-- +goose StatementBegin
+alter table payment_cred
+    add column id bigserial;
+
+alter table payment_cred
+    drop constraint payment_cred_pkey;
+
+alter table payment_cred
+    add primary key (id);
+
+alter table payment_cred
+    add column id_default bool;
+
+alter table students
+    add column payment_id bigint;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+
+-- +goose StatementEnd
